897aa1482a79a5b7748faf66900aa9909153486a
Greg Reinbach <greg@reinbach.com>
2014-12-17 09:14:10 -0500

    able to send messages

--- finance/core/views.py
+++ finance/core/views.py
@@ -2,11 +2,25 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.core.urlresolvers import reverse
 from django.views.generic import TemplateView, FormView
-from finance.core.forms import RegisterForm
+from finance.core.forms import RegisterForm, ContactForm
 
 
 class HomeView(TemplateView):
     template_name = "home.html"
+
+
+class ContactView(FormView):
+    template_name = "contact.html"
+    form_class = ContactForm
+
+    def get_success_url(self):
+        return reverse("contact")
+
+    def form_valid(self, form):
+        form.send_email()
+        messages.success(self.request,
+                         u"Your message has been sent. Thank you!")
+        return super(ContactView, self).form_valid(form)
 
 
 class RegisterView(FormView):
--- finance/urls.py
+++ finance/urls.py
@@ -2,13 +2,14 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-from finance.core.views import HomeView, RegisterView
+from finance.core.views import HomeView, RegisterView, ContactView
 
 
 urlpatterns = patterns(
     '',
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^register/$', RegisterView.as_view(), name='register'),
+    url(r'^contact/$', ContactView.as_view(), name='contact'),
     url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
