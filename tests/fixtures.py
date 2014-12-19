927d843a420b80e3ddeab185e4a64f9c2782fcd9
Greg Reinbach <greg@reinbach.com>
2014-12-19 17:29:58 -0500

    updated fixtures based on profile addition

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,14 +1,23 @@
 from django_dynamic_fixture import G
 from finance.accounts.models import Account, AccountType, Transaction
+from finance.core.models import Profile
+
+
+def profile_factory(**kwargs):
+    return G(Profile, **kwargs)
 
 
 def account_type_factory(**kwargs):
+    if "profile" not in kwargs:
+        kwargs["profile"] = profile_factory()
     return G(AccountType, **kwargs)
 
 
 def account_factory(**kwargs):
     if "account_type" not in kwargs:
         kwargs["account_type"] = account_type_factory()
+    if "profile" not in kwargs:
+        kwargs["profile"] = kwargs["account_type"].profile
     return G(Account, **kwargs)
 
 
