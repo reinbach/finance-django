389bc756548e19f9fcd4e442f07c3c348bf4742e
Greg Reinbach <greg@reinbach.com>
2014-12-21 15:58:18 -0500

    added transactions import model tests

--- tests/accounts/test_models.py
+++ tests/accounts/test_models.py
@@ -1,8 +1,11 @@
 import datetime
+import os
 import pytest
 
 from decimal import Decimal
-from finance.accounts.models import Account, AccountType, Transaction
+from django.conf import settings
+from finance.accounts.models import (Account, AccountType, Transaction,
+                                     TransactionsImport)
 from tests.fixtures import (account_factory, account_type_factory,
                             transaction_factory)
 
@@ -65,3 +68,105 @@ class TestTransaction():
         t = Transaction(account_debit=a1, account_credit=a2, amount="10.00",
                         summary="quick trx")
         assert unicode(t) == "quick trx 10.00"
+
+
+@pytest.mark.django_db
+class TestTransactionsImport():
+    TEST_FILE = os.path.join(settings.BASE_DIR, "tests/import_test_sample.csv")
+
+    def test_init(self):
+        a = account_factory()
+        t = TransactionsImport(a.pk, self.TEST_FILE)
+        assert t.main_account_pk == a.pk
+        assert t.filename == self.TEST_FILE
+        assert t.transactions == []
+
+    def test_is_duplicate(self):
+        trx = {"summary": "sum", "amount": "10.00", "date": "1/1/2010"}
+        transaction_factory(summary=trx["summary"],
+                            amount=trx["amount"], date="2010-01-01")
+        a = account_factory()
+        t = TransactionsImport(a.pk, self.TEST_FILE)
+        assert t.is_duplicate(trx) is True
+
+    def test_is_duplicate_false(self):
+        trx = {"summary": "sum", "amount": "10.00", "date": "01/01/2010"}
+        transaction_factory(summary=trx["summary"],
+                            amount=trx["amount"])
+        a = account_factory()
+        t = TransactionsImport(a.pk, self.TEST_FILE)
+        assert t.is_duplicate(trx) is False
+
+    def test_get_account_debit(self):
+        acct1 = account_factory()
+        acct2 = account_factory()
+        transaction_factory(account_debit=acct1, account_credit=acct2,
+                            summary="sum")
+        t = TransactionsImport(acct1.pk, self.TEST_FILE)
+        assert t.get_account("sum") == acct2.pk
+
+    def test_get_account_credit(self):
+        acct1 = account_factory()
+        acct2 = account_factory()
+        transaction_factory(account_debit=acct1, account_credit=acct2,
+                            summary="sum")
+        t = TransactionsImport(acct2.pk, self.TEST_FILE)
+        assert t.get_account("sum") == acct1.pk
+
+    def test_get_account_none(self):
+        acct1 = account_factory()
+        acct2 = account_factory()
+        transaction_factory(account_debit=acct1, account_credit=acct2,
+                            summary="sum")
+        t = TransactionsImport(acct2.pk, self.TEST_FILE)
+        assert t.get_account("sumelse") is None
+
+    def test_set_accounts_debit(self):
+        acct1 = account_factory()
+        acct2 = account_factory()
+        transaction_factory(account_debit=acct1, account_credit=acct2,
+                            summary="sum")
+        trx = {"summary": "sum", "amount": "10.00", "date": "01/01/2010"}
+        t = TransactionsImport(acct1.pk, self.TEST_FILE)
+        t.set_accounts(trx)
+        assert trx["account_debit"] == acct1.pk
+        assert trx["account_credit"] == acct2.pk
+        assert trx["amount"] == "10.00"
+
+    def test_set_accounts_credit(self):
+        acct1 = account_factory()
+        acct2 = account_factory()
+        transaction_factory(account_debit=acct1, account_credit=acct2,
+                            summary="sum")
+        trx = {"summary": "sum", "amount": "-10.00", "date": "01/01/2010"}
+        t = TransactionsImport(acct1.pk, self.TEST_FILE)
+        t.set_accounts(trx)
+        assert trx["account_debit"] == acct2.pk
+        assert trx["account_credit"] == acct1.pk
+        assert trx["amount"] == "10.00"
+
+    def test_map_field(self):
+        trx_import = {"Post Date": "2010-01-01", "Description": "sum",
+        "Amount": "15.25"}
+        acct1 = account_factory()
+        t = TransactionsImport(acct1.pk, self.TEST_FILE)
+        trx = t.map_fields(trx_import)
+        assert trx["date"] == "2010-01-01"
+        assert trx["summary"] == "sum"
+        assert trx["amount"] == "15.25"
+
+    def test_parse_file(self):
+        acct = account_factory()
+        t = TransactionsImport(acct.pk, self.TEST_FILE)
+        t.parse_file()
+        assert len(t.transactions) == 2
+
+    def test_parse_file_duplicate(self):
+        acct = account_factory()
+        transaction_factory(account_credit=acct, summary="SPICE & GRAIN",
+                            amount="76.35", date="2013-02-22")
+        t = TransactionsImport(acct.pk, self.TEST_FILE)
+        t.parse_file()
+        assert len(t.transactions) == 2
+        assert t.transactions[0]["duplicate"] is True
+        assert t.transactions[1]["duplicate"] is False
