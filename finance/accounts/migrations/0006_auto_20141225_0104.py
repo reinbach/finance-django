59da8cc42d82f9aa08d23af20aa85a5d7bfc0506
Greg Reinbach <greg@reinbach.com>
2014-12-24 20:05:18 -0500

    migration for ordering on account table

---
+++ finance/accounts/migrations/0006_auto_20141225_0104.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts', '0005_auto_20141224_1917'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='account',
+            options={'ordering': ['account_type', 'name']},
+        ),
+    ]
